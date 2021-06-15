4832288cbcd1955926995f9f1f914fd2cb637ac2
abrahamLillo26 <abr.lillo@duocuc.cl>
2021-06-15 09:41:42 -0400

    proyecto con crud faltan cosas

--- noticiasDjango/miApp/models.py
+++ noticiasDjango/miApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Noticias(models.Model):
+    tituloNoticia = models.CharField(max_length=75)
+    descripcioCorta = models.CharField(max_length=100)
+    descripcioLarga = models.CharField(max_length=1000)
+    fechaNoticia = models.DateField()
+    imagenNoticia = models.ImageField(upload_to="productos", null=True)
+
+    def __str__(self):
+        return self.tituloNoticia
--- noticiasDjango/miApp/views.py
+++ noticiasDjango/miApp/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.db.models.fields import files
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Noticias
+from .forms import NoticiaForm
+
 
 # Create your views here.
 def home(request):
-
-    return render(request, 'miApp/home.html')
+    noticias = Noticias.objects.all()
+    data = {
+        'noticias': noticias
+    }
+    return render(request, 'miApp/home.html', data)
 
 
 def contacto(request):
@@ -30,3 +37,54 @@ def tarjetaNoticia(request):
 
     return render(request, 'miApp/tarjetaNoticia.html')
 
+
+def agregarNoticia(request):
+
+    data = {
+        'form': NoticiaForm()
+    }
+
+    if request.method == 'POST':
+        formulario = NoticiaForm(data=request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"] = "guardado correctamente"
+        else:
+            data["mensaje"] = " no guardado"
+
+    return render(request, 'miApp/crudNoticia/agregarNoticia.html', data)
+
+
+
+def listarNoticias(request):
+
+    noticias = Noticias.objects.all()
+
+    data = {
+        'noticias': noticias
+    }
+    
+    return render(request, 'miApp/crudNoticia/listarNoticias.html', data)
+
+
+
+def modificarNoticia(request, id):
+    noticia = get_object_or_404(Noticias, id=id)
+
+    data = {
+        'form': NoticiaForm(instance=noticia)
+    }
+
+    if request.method == 'POST':
+        formulario = NoticiaForm(data=request.POST, instance=noticia, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(to="listarNoticias")
+        
+    return render(request, 'miApp/crudNoticia/modificarNoticia.html', data)
+
+
+def eliminarNoticia(request, id):
+    noticia = get_object_or_404(Noticias, id=id)
+    noticia.delete()
+    return redirect(to="listarNoticias")
